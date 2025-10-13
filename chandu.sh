a72818a1823c8c02a988d7ae95dd36b8cef96764
chandrasekhartelukuntla <chandrasekhartelukuntla21145@gmail.com>
2025-10-13 12:10:27 +0530

    hi

--- chandu.sh
+++
@@ -1,3 +0,0 @@
-#! bin/bash
-
-hi this is chandrasekhar
---
+++ special-variable.sh
@@ -0,0 +1,10 @@
+#! bin/bash
+
+echo "All variables : $@"
+echo "Number of variables passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Host name: $HOSTNAME"
+echo "Process ID of the current shell script: $$"
