1b37ee8676dc5ab1fffa18fc5d137612b6c73ba8
chandrasekhartelukuntla <chandrasekhartelukuntla21145@gmail.com>
2025-10-13 15:26:26 +0530

    this is my first commit

---
+++ 09 functions.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+#! /bin/bash
+
+USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2..FAILURE"
+        exit 1
+    else
+        echo "$2..SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then 
+    echo "please run this script with root access."
+    exit 1 # manually exit if error comes.
+else   
+    echo "you are super user."
+fi
+
+dnf install mysql -y
+
+VALIDATE $? "Installing mysql"
+
+dnf install git -y
+
+VALIDATE $? "Installing git"
+
+
